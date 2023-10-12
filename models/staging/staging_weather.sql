cb3370d76faeb9673f7bf7e884a91ef93f897532
NicoTesta86 <nico.atesta@gmail.com>
2023-10-12 16:34:04 +0200

    ok

--- models/staging/staging_weather.sql
+++ models/staging/staging_weather.sql
@@ -1,7 +1,7 @@
 WITH temperature_daily AS (
     SELECT ((data -> 'forecast' -> 'date')::VARCHAR)::date  AS date,
-        ((data -> 'forecast' ->  'day' -> 'maxtemp_c')::VARCHAR)::FLOAT AS maxtemp_c,
-        ((data -> 'forecast' ->  'day' -> 'mintemp_c')::VARCHAR)::FLOAT AS mintemp_c,
+        ((data -> 'forecast' -> 'day' -> 'maxtemp_c')::VARCHAR)::FLOAT AS maxtemp_c,
+        ((data -> 'forecast' -> 'day' -> 'mintemp_c')::VARCHAR)::FLOAT AS mintemp_c,
         ((data -> 'forecast' -> 'day' -> 'avgtemp_c')::VARCHAR)::FLOAT AS avgtemp_c,
         (data -> 'location' -> 'name')::VARCHAR  AS city,
         (data -> 'location' -> 'region')::VARCHAR  AS region,

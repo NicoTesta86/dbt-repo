49a0d164f4e043281ac95894b353a3a1a40f606f
NicoTesta86 <nico.atesta@gmail.com>
2023-10-13 14:32:52 +0200

    test

--- models/mart/weekday_avg_temp.sql
+++ models/mart/weekday_avg_temp.sql
@@ -1,6 +1,6 @@
 WITH total_avg AS (
     SELECT
-        city, country, year, weekday,
+        city, country, date, weekday,
         AVG(avgtemp_c) AS avg_temp_weekday,
         MAX(maxtemp_c) AS max_temp_weekday,
         MIN(mintemp_c) AS min_temp_weekday

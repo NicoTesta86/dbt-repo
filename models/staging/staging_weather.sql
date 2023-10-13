a57aa5391b0011fd12c224a760fb8534a1140bae
NicoTesta86 <nico.atesta@gmail.com>
2023-10-13 15:31:29 +0200

    pull

--- models/staging/staging_weather.sql
+++ models/staging/staging_weather.sql
@@ -1,15 +1,16 @@
 WITH temperature_daily AS (
-    SELECT ((data -> 'forecast' -> 'date')::VARCHAR)::date  AS date,
-        ((data -> 'forecast' -> 'day' -> 'maxtemp_c')::VARCHAR)::FLOAT AS maxtemp_c,
-        ((data -> 'forecast' -> 'day' -> 'mintemp_c')::VARCHAR)::FLOAT AS mintemp_c,
-        ((data -> 'forecast' -> 'day' -> 'avgtemp_c')::VARCHAR)::FLOAT AS avgtemp_c,
-        (data -> 'location' -> 'name')::VARCHAR  AS city,
-        (data -> 'location' -> 'region')::VARCHAR  AS region,
-        (data -> 'location' -> 'country')::VARCHAR  AS country,
-        ((data -> 'location' -> 'lat')::VARCHAR)::NUMERIC  AS lat, 
-        ((data -> 'location' -> 'lon')::VARCHAR)::NUMERIC  AS lon
+        SELECT ((data -> 'forecast' -> 'forecastday' -> 0 -> 'date')::VARCHAR)::date AS date,
+        ROUND(((data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'maxtemp_c')::VARCHAR)::NUMERIC, 2) AS maxtemp_c,
+        ROUND(((data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'mintemp_c')::VARCHAR)::NUMERIC, 2) AS mintemp_c,
+        ROUND(((data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'avgtemp_c')::VARCHAR)::NUMERIC, 2) AS avgtemp_c,
+        ROUND(((data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'maxwind_kph')::VARCHAR)::NUMERIC, 2) AS maxwind_kph,
+        ROUND(((data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'avgvis_km')::VARCHAR)::NUMERIC, 2) AS avgvis_km,
+        ROUND(((data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'avghumidity')::VARCHAR)::NUMERIC, 2) AS avghumidity,
+        (data -> 'location' -> 'name')::VARCHAR AS city,
+        (data -> 'location' -> 'region')::VARCHAR AS region,
+        (data -> 'location' -> 'country')::VARCHAR AS country,
+        ((data -> 'location' -> 'lat')::VARCHAR)::NUMERIC AS lat,
+        ((data -> 'location' -> 'lon')::VARCHAR)::NUMERIC AS lon
     FROM {{source("staging", "raw_temp")}})
-SELECT * 
+SELECT *
 FROM temperature_daily
-
-

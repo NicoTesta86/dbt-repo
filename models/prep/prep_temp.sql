889dc711293e18f1a684e2ee05230ea99a52cc76
NicoTesta86 <nico.atesta@gmail.com>
2023-10-13 10:37:05 +0200

    push

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -1,7 +1,11 @@
-with temp_daily as (
-    select *,
-        to_char(timestamp, 'Day') as weekday
-    from {{ref("staging_temp")}}
+WITH temp_daily AS (
+    SELECT * 
+    FROM {{ref('staging_temp')}}
+),
+add_weekday AS (
+    SELECT *,
+        to_char(timestamp, 'Day') AS weekday,
+    FROM temp_daily
 )
-select * 
-from temp_daily
+SELECT *
+FROM add_weekday

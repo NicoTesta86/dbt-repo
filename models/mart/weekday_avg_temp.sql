425a2e614a784b0d205945fff7781b72ccf07820
NicoTesta86 <nico.atesta@gmail.com>
2023-10-13 11:02:20 +0200

    weekpl

---
+++ models/mart/weekday_avg_temp.sql
@@ -0,0 +1,10 @@
+WITH avg_temp_per_country AS (
+    SELECT
+        (data -> 'location' -> 'country')::VARCHAR AS country,
+        AVG(((data -> 'forecast' -> 'day' -> 'avgtemp_c')::VARCHAR)::FLOAT) AS avg_avgtemp_c
+    FROM {{source("staging", "raw_temp")}}
+    GROUP BY (data -> 'location' -> 'country')::VARCHAR
+)
+
+SELECT *
+FROM avg_temp_per_country;

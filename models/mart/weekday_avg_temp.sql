a9302395f0cd365b0a5909836eba1b148051b756
NicoTesta86 <nico.atesta@gmail.com>
2023-10-13 14:10:00 +0200

    test

--- models/mart/weekday_avg_temp.sql
+++ models/mart/weekday_avg_temp.sql
@@ -1,7 +1,7 @@
 WITH weekday_avg_temp AS (
     SELECT
         city,
-        AVG(avgtemp_c) AS avg_avgtemp_c
+        AVG('Berlin') AS avg_avgtemp_c
     FROM {{ref('prep_temp')}}
     GROUP BY city
 )

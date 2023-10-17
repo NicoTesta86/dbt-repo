c176b65340c8e0cf9c20df42049e8d7f9b66a01c
NicoTesta86 <nico.atesta@gmail.com>
2023-10-17 15:21:16 +0200

    ok

--- models/mart/weekday_avg_temp.sql
+++ models/mart/weekday_avg_temp.sql
@@ -4,6 +4,9 @@ WITH total_avg AS (
         AVG(avgtemp_c) AS avg_temp_weekday,
         MAX(maxtemp_c) AS max_temp_weekday,
         MIN(mintemp_c) AS min_temp_weekday
+        MAX(maxwind_kph) AS max_wind_kph
+        AVG(avgvis_km) AS avg_vis_km
+        AVG(avghumidity) AS avg_umidity
     FROM {{ref('prep_temp')}}
     GROUP BY city, country, date, weekday
 )

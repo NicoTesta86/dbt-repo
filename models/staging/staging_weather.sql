c44166c6a0fdec7dfbfb3e07ca134dc3ec638953
NicoTesta86 <nico.atesta@gmail.com>
2023-10-17 15:39:43 +0200

    ok

--- models/staging/staging_weather.sql
+++ models/staging/staging_weather.sql
@@ -1,26 +1,16 @@
 WITH temperature_daily AS (
-    SELECT ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'date')::VARCHAR)::date AS date,
-        ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'maxtemp_c')::VARCHAR)::FLOAT AS maxtemp_c,
-        ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'mintemp_c')::VARCHAR)::FLOAT AS mintemp_c,
-        ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'avgtemp_c')::VARCHAR)::FLOAT AS avgtemp_c,
-        (extracted_data -> 'location' -> 'name')::VARCHAR AS city,
-        (extracted_data -> 'location' -> 'region')::VARCHAR AS region,
-        (extracted_data -> 'location' -> 'country')::VARCHAR AS country,
-        ((extracted_data -> 'location' -> 'lat')::VARCHAR)::NUMERIC AS lat,
-        ((extracted_data -> 'location' -> 'lon')::VARCHAR)::NUMERIC AS lon
-    FROM {{source("staging", "raw_temp")}}),
-        temperatutre_daily_updated AS (
-            SELECT
-                date,
-                maxtemp_c as maxtemp_c,
-                mintemp_c,
-                avgtemp_c,
-                substring(city, 2, (length(city)-2)) as city,
-                substring(region, 2, (length(region)-2)) as region,
-                substring(country, 2, (length(country)-2)) as country,
-                lat,
-                lon
-            FROM temperature_daily
-)
+        SELECT ((data -> 'forecast' -> 'forecastday' -> 0 -> 'date')::VARCHAR)::date AS date,
+        ((data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'maxtemp_c')::VARCHAR)::FLOAT AS maxtemp_c,
+        ((data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'mintemp_c')::VARCHAR)::FLOAT AS mintemp_c,
+        ((data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'avgtemp_c')::VARCHAR)::FLOAT AS avgtemp_c,
+        ((data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'maxwind_kph')::VARCHAR)::FLOAT AS maxwind_kph,
+        ((data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'avgvis_km')::VARCHAR)::FLOAT AS avgvis_km,
+        ((data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'avghumidity')::VARCHAR)::FLOAT AS avghumidity,
+        REPLACE((data -> 'location' -> 'name')::VARCHAR, '"', '') AS city,
+        REPLACE((data -> 'location' -> 'region')::VARCHAR, '"', '') AS region,
+        REPLACE((data -> 'location' -> 'country')::VARCHAR, '"', '') AS country,
+        ((data -> 'location' -> 'lat')::VARCHAR)::NUMERIC AS lat,
+        ((data -> 'location' -> 'lon')::VARCHAR)::NUMERIC AS lon
+    FROM {{source("staging", "raw_temp")}})
 SELECT *
-FROM temperatutre_daily_updated
+FROM temperature_daily

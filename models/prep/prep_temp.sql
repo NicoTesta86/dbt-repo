de7b730cd0a7d322f7e386f4b44d70e33438ef3c
NicoTesta86 <nico.atesta@gmail.com>
2023-10-17 15:34:29 +0200

    ok

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -5,7 +5,10 @@ WITH temp_daily AS (
 add_weekday AS (
     SELECT *,
         to_char(date, 'Day') AS weekday,
-        to_char(date, 'DD') AS day_num
+        to_char(date, 'DD') AS day_num,
+        to_char(date, 'Month') AS month,
+        to_char(date, 'IYYY') AS year,
+        to_char(date, 'WW') AS week
     FROM temp_daily
 )
 SELECT *

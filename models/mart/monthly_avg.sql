629e3d38765d798a3b27c4fab199d6dbc8540b5a
NicoTesta86 <nico.atesta@gmail.com>
2023-10-18 15:20:01 +0200

    push

---
+++ models/mart/monthly_avg.sql
@@ -0,0 +1,11 @@
+WITH monthly_avg AS (
+    SELECT
+        city, country, year, month, lat, lon
+        AVG(avgtemp_c) AS avg_temp_month,
+        MAX(maxtemp_c) AS max_temp_month,
+        MIN(mintemp_c) AS min_temp_month
+    FROM {{ref('prep_temp')}}
+    GROUP BY city, country, year, month, lat, lon
+)
+SELECT *
+FROM monthly_avg
